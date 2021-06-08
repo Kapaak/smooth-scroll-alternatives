2480641249724174f62750140292388658de12ca
Pavel Zapletal <wardreth@gmail.com>
2021-06-08 14:00:30 +0200

    latest

--- pages/package/index.js
+++ pages/package/index.js
@@ -8,8 +8,9 @@ const index = () => {
 			<br />
 			<h1>Basic smooth scroll</h1>
 			<p>
-				This is the basic version of smooth scroll,it uses href tags with ids.
-				Not working in Safari and IE, but its very easy to implement.
+				This is the most advanced version of smooth scroll,it uses react-scroll
+				package and I can change the scroll duration and more. Working in Safari
+				and IE and it takes some time to understand.
 			</p>
 
 			<Button>
@@ -27,7 +28,7 @@ const index = () => {
 			<Section bgcol="#ababab" id="first-basic" />
 			<Section id="second-basic" />
 			<Button>
-				<a href="#top-basic">scroll top</a>
+				<a onClick={() => scroll.scrollToTop({ duration: 2000 })}>scroll top</a>
 			</Button>
 		</div>
 	);
