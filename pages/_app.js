036c3c24d18a651b12257327f4267e7ddfc09436
Pavel Zapletal <wardreth@gmail.com>
2021-06-08 13:04:34 +0200

    dd

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import Navbar from "../components/Navbar";
 import GlobalStyles from "../styles/GlobalStyles";
-import smoothscroll from "smoothscroll-polyfill";
 
 function MyApp({ Component, pageProps }) {
-	smoothscroll.polyfill();
-	console.log("polyfilled");
-
 	return (
 		<>
 			<GlobalStyles />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+import smoothscroll from "smoothscroll-polyfill";
+
 export default function Home() {
+	useEffect(() => smoothscroll.polyfill());
+	console.log("polyfilled");
 	return (
 		<>
 			<h1>xx</h1>
