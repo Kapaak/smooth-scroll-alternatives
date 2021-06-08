4b62011726948c88926296d3242436575a8314a8
Pavel Zapletal <wardreth@gmail.com>
2021-06-08 13:12:32 +0200

    aa

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
-import { useEffect } from "react";
-import smoothscroll from "smoothscroll-polyfill";
-
 export default function Home() {
-	useEffect(() => smoothscroll.polyfill());
-	console.log("polyfilled");
 	return (
 		<>
 			<h1>xx</h1>
