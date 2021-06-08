4f60a8fb71199728d6edf10e17752865e9ffb70b
Pavel Zapletal <wardreth@gmail.com>
2021-06-08 12:52:17 +0200

    added polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import Navbar from "../components/Navbar";
 import GlobalStyles from "../styles/GlobalStyles";
+import smoothscroll from "smoothscroll-polyfill";
 
 function MyApp({ Component, pageProps }) {
+	smoothscroll.polyfill();
+
 	return (
 		<>
 			<GlobalStyles />
